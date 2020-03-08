6c51c0f29280dcd164350f16720a62cd79e587c6
merelwemelsfelder <merel@wemel.nl>
2020-03-08 10:18:17 +0100

    added timeslider and re-organized stuff in folders

--- frontend/slider.js
+++
@@ -1,7 +0,0 @@
-var slider_single = document.getElementById("myRange");
-var output = document.getElementById("slider_demo");
-output.innerHTML = slider_single.value;
-
-slider_single.oninput = function() {
-  output.innerHTML = this.value;
-}
