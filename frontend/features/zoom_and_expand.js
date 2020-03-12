c5efe674998aa12798079d9987457ee6656c3e32
LyuFangzheng <lyufangzheng@gmail.com>
2020-03-12 06:28:00 +0100

    Update zoom_and_expand.js

--- frontend/features/zoom_and_expand.js
+++ frontend/features/zoom_and_expand.js
@@ -1,48 +1,49 @@
+//       <div class="container">
+//             <h2>Dropdown</h2>                                        
+//             <div id="option">
+//             <input name="updateButton"
+//                 type="button"
+//                 value="Back To Upper Layer"
+//                 onclick="update()"
+//             />
+//             </div> 
 
-var width = 1100,
-  height = 700,
-  expand = {},
-  layer,
-  root;
+      var width = 1100,
+          height = 500,
+          expand = {},
+          layer,
+          node_total = [],
+          link_total = [],
+          root;
 
-var curve = d3.svg.line()
-  .interpolate("cardinal-closed")
-  .tension(.85);
+      var curve = d3.svg.line()
+        .interpolate("cardinal-closed")
+        .tension(.85);
 
-var fill = d3.scale.category20();
+      var fill = d3.scale.category20();
 
+      var svg = d3.select("body")
+          .append("svg")
+          .attr("width", width)
+          .attr("height", height);
 
-function getGroup(n) { 
-  return n.group; 
-}
+      var force = d3.layout.force()
+          .size([width, height]);
 
+      var zoom = d3.behavior.zoom()
+                .scaleExtent([1, 8])
+                .on("zoom", zoomed);  
 
-// var force = d3.layout.force()
-//     .size([width, height])
-//     .on("tick", tick);
-
-var svg = d3.select("body").append("svg")
-  .attr("width", width)
-  .attr("height", height);
-
-var force = d3.layout.force()
-  .size([width, height])
-  .on("tick", tick);
-
-var zoom = d3.behavior.zoom()
-        .scaleExtent([1, 8])
-        .on("zoom", zoomed);  
-
-var link = svg.selectAll(".link"),
-    node = svg.selectAll(".node");
-
-svg
-// .call(zoom) // delete this line to disable free zooming
-  .call(zoom.event);
+      var link = svg.selectAll(".link"),
+          node = svg.selectAll(".node");
 
 
+      svg
+        // .call(zoom) // delete this line to disable free zooming
+        .call(zoom.event);
 
-d3.json("../data/output_example_reorg.json", function(error, json) {
+
+      d3.json("../data/output_example_reorg.json", function(error, json) {
         if (error) throw error;
 
         network = json;        
@@ -68,6 +69,7 @@ d3.json("../data/output_example_reorg.json", function(error, json) {
             edges_layer1 = network.networks[1].edges;
             edges_layer2 = network.networks[0].edges;
 
+        console.log(nodes_initial);
 
         edges_initial.forEach(function(d, i) {
             d.source = nodes_initial[d.origin_node_id];
@@ -80,10 +82,11 @@ d3.json("../data/output_example_reorg.json", function(error, json) {
         });
 
         edges_layer2.forEach(function(d, i) {
-          // if(nodes_layer2[d.origin_node_id] in )
           d.source = nodes_layer2[d.origin_node_id];
           d.target = nodes_layer2[d.destination_node_id];
         });
+
+        console.log(edges_initial);
   
         link = link.data(edges_initial, function(d) { return d.target.id; });
 
@@ -137,11 +140,6 @@ d3.json("../data/output_example_reorg.json", function(error, json) {
 
           console.log(d.id);
 
-          // d3.selectAll('.node') 
-          //   .attr("class","hidden");
-
-          // d3.selectAll('.link')
-          //   .attr("class","hidden");
           if (force) force.stop();
           svg.selectAll('.node').remove();
           svg.selectAll('.link').remove();
@@ -194,27 +192,42 @@ d3.json("../data/output_example_reorg.json", function(error, json) {
               }
             }
           }
-          console.log(link_new);  
-          console.log(node_new);
+          console.log(link_new);
+          console.log(node_new);  
+          // console.log(node_new[17]);
 
-          // var edges_final = [];
-          // link_new.forEach(function(e) { 
-          //   var sourceNode = node_new.filter(function(n) { return n.id === e.origin_node_id; })[0],
-          //       targetNode = node_new.filter(function(n) { return n.id === e.destination_node_id; })[0];
-              
-          //   edges_final.push({source: sourceNode, target: targetNode});
-          // });
+          var edges_final = new Array();
+          for (var i = link_new.length - 1; i >= 0; i--) {
+            
+            var sourceNode = {};
+            var targetNode = {};
 
-          // node_new.forEach(function(n){ console.log(n)});
+            for (var j = node_new.length - 1; j >= 0; j--) {
+              if(link_new[i].origin_node_id == node_new[j].id){
+                sourceNode = node_new[j];
+              }
+            }
 
-          console.log(edges_final);
+            for (var j = node_new.length - 1; j >= 0; j--) {
+              if(link_new[i].destination_node_id == node_new[j].id){
+                targetNode = node_new[j];
+              }
+            }
+
+            if(sourceNode.id && targetNode.id){
+
+            edges_final.push({
+              source: sourceNode,
+              target: targetNode
+            });
+            }
+          }
+
+          console.log(edges_final[17]);
 
           link = link.data(edges_final, function(d) { return d.target.id; });
 
-          // Exit any old links.
-          // link.exit().remove();
-
-          // Enter any new links.
+          // // Enter any new links.
           link.enter().insert("line", ".node")
               .attr('name', function(d) { return d.id; })
               .attr("class", "link")
@@ -253,6 +266,37 @@ d3.json("../data/output_example_reorg.json", function(error, json) {
             .on("tick", tick)
             .start();
 
+          // var force_new = d3.layout.force()
+          //       .nodes(node_new) //指定节点数组
+          //       .links(edges_final) //指定连线数组
+          //       .size([width,height]) //指定作用域范围
+          //       .linkDistance(150) //指定连线长度
+          //       .charge([-400]); 
+
+          //     force_new.start();
+
+
+
+         //  var svg_edges = svg.selectAll("line")
+         //    .data(edges_final)
+         //    .enter()
+         //    .append("line")
+         //    .style("stroke","#ccc")
+         //    .style("stroke-width",1);
+         
+         //  var color = d3.scale.category20();
+         
+         // //添加节点 
+         //  var svg_nodes = svg.selectAll("circle")
+         //    .data(node_new)
+         //    .enter()
+         //    .append("circle")
+         //    .attr("r",20)
+         //    .style("fill",function(d,i){
+         //      return color(i);
+         //    })
+         //    .call(force.drag);  //使得节点能够拖动
+
         }
 
         function tick() {
@@ -276,5 +320,3 @@ d3.json("../data/output_example_reorg.json", function(error, json) {
         svg.style("stroke-width", 1.5 / d3.event.scale + "px");
         svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
       } 
-
-
