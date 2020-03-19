5aa894b5cf0be3cd396f1a3ecb12d8df8d5865a8
CharlotteSmoor <charlottesmoor@gmail.com>
2020-03-19 14:56:52 +0100

    front page edits

--- frontend/features/collaps.js
+++ frontend/features/collaps.js
@@ -13,3 +13,18 @@ for (i = 0; i < coll.length; i++) {
     } 
   });
 }
+
+var innercoll = document.getElementsByClassName("innercollapsible");
+var i;
+
+for (i = 0; i < innercoll.length; i++) {
+  innercoll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
